a1bec4f963c5e56f97169ced4de8593e4b514ec6
Pedro Henrique de Mesquita Braga <104701771+PHMesquita97@users.noreply.github.com>
2024-01-29 02:23:38 -0300

    inicio media queries -
    - Alterações no HTML.
    - Mudanças no CSS
    - Início da responsividade para desktop

---
+++ scripts.js
@@ -0,0 +1,9 @@
+<script>
+        function clickMenu() {
+    if (burger.style.display == 'block') {
+        burger.style.display = 'none'
+    } else {
+        burger.style.display = "block"
+    }
+}
+    </script>
