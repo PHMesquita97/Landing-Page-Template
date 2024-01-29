8b9c1546d4e249c551d0d768cfba35ee689d15e0
Pedro Henrique de Mesquita Braga <104701771+PHMesquita97@users.noreply.github.com>
2024-01-29 17:12:33 -0300

    Início JavaScript.
    adicionado o javaScript para:
    - Menu nav
    - Menu Footer (mobile)
    - Cartões das FAQs
    
    Mudanças no css para responsividade.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
-<script>
-        function clickMenu() {
+function clickMenu() {
     if (burger.style.display == 'block') {
         burger.style.display = 'none'
     } else {
         burger.style.display = "block"
     }
 }
-    </script>
+
+function clickArrow(button) {
+    // Encontre o elemento irmão com a classe 'footer_menu_itens_show' do botão clicado
+    var menuToShow = button.parentNode.nextElementSibling;
+
+    if (window.getComputedStyle(menuToShow).display === 'flex') {
+        menuToShow.style.display = 'none';
+    } else {
+        menuToShow.style.display = 'flex';
+    }
+
+}
+
+function toggleAnswer(button) {
+    var answer = button.parentNode.nextElementSibling;
+
+    if (window.getComputedStyle(answer).display === 'none') {
+        answer.style.display = 'block';
+        button.src = "./assets/icons/Minus.svg";
+    } else {
+        answer.style.display = 'none';
+        button.src = "./assets/icons/Plus.svg";
+    }
+}
+
